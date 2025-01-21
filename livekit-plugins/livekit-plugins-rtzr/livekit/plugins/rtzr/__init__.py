0418ceb9dfb595923ccf552be00b4b1f6735dc3c
hsjun99 <hsjun999@gmail.com>
2025-01-21 17:26:44 +0900

    add protos

--- livekit-plugins/livekit-plugins-rtzr/livekit/plugins/rtzr/__init__.py
+++ livekit-plugins/livekit-plugins-rtzr/livekit/plugins/rtzr/__init__.py
@@ -1,8 +1,17 @@
 from .stt import STT
 from .models import RTZRLanguages, RTZRModels
 from .version import __version__
+from . import vito_stt_client_pb2
+from . import vito_stt_client_pb2_grpc
 
-__all__ = ["STT", "RTZRLanguages", "RTZRModels", "__version__"]
+__all__ = [
+    "STT",
+    "RTZRLanguages",
+    "RTZRModels",
+    "__version__",
+    "vito_stt_client_pb2",
+    "vito_stt_client_pb2_grpc",
+]
 
 from livekit.agents import Plugin
 
