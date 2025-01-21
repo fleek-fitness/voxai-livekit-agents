00cecf7a553673ed5aae782eebaff6a551edcb19
hsjun99 <hsjun999@gmail.com>
2025-01-21 17:35:58 +0900

    fix

--- livekit-plugins/livekit-plugins-rtzr/livekit/plugins/rtzr/vito_stt_client_pb2_grpc.py
+++ livekit-plugins/livekit-plugins-rtzr/livekit/plugins/rtzr/vito_stt_client_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import vito_stt_client_pb2 as vito__stt__client__pb2
 
-GRPC_GENERATED_VERSION = '1.67.1'
+GRPC_GENERATED_VERSION = '1.69.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
