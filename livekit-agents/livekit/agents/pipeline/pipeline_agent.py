e5910b2e55728e6473e0ebe95b485b5ae6717103
hsjun99 <hsjun999@gmail.com>
2025-01-21 11:49:31 +0900

    change FINAL_TRANSCRIPT_TIMEOUT: 5 -> 1

--- livekit-agents/livekit/agents/pipeline/pipeline_agent.py
+++ livekit-agents/livekit/agents/pipeline/pipeline_agent.py
@@ -896,9 +896,9 @@ class VoicePipelineAgent(utils.EventEmitter[EventTypes]):
                 return
 
             assert isinstance(speech_handle.source, LLMStream)
-            assert not user_question or speech_handle.user_committed, (
-                "user speech should have been committed before using tools"
-            )
+            assert (
+                not user_question or speech_handle.user_committed
+            ), "user speech should have been committed before using tools"
 
             llm_stream = speech_handle.source
 
@@ -1048,9 +1048,9 @@ class VoicePipelineAgent(utils.EventEmitter[EventTypes]):
         speech_id: str,
         source: str | LLMStream | AsyncIterable[str],
     ) -> SynthesisHandle:
-        assert self._agent_output is not None, (
-            "agent output should be initialized when ready"
-        )
+        assert (
+            self._agent_output is not None
+        ), "agent output should be initialized when ready"
 
         tk = SpeechDataContextVar.set(SpeechData(speech_id))
 
@@ -1201,7 +1201,7 @@ class _DeferredReplyValidation:
     PUNCTUATION = ".!?"
     PUNCTUATION_REDUCE_FACTOR = 0.75
 
-    FINAL_TRANSCRIPT_TIMEOUT = 5
+    FINAL_TRANSCRIPT_TIMEOUT = 1
 
     def __init__(
         self,
