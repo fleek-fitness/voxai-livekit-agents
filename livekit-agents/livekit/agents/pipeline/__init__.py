81448e3f76c9b759d3d3f5686a25578d705c9317
hsjun99 <hsjun999@gmail.com>
2025-02-11 14:36:46 +0900

    fix

--- livekit-agents/livekit/agents/pipeline/__init__.py
+++ livekit-agents/livekit/agents/pipeline/__init__.py
@@ -2,10 +2,12 @@ from .pipeline_agent import (
     AgentCallContext,
     AgentTranscriptionOptions,
     VoicePipelineAgent,
+    FlowVoicePipelineAgent,
 )
 
 __all__ = [
     "VoicePipelineAgent",
+    "FlowVoicePipelineAgent",
     "AgentCallContext",
     "AgentTranscriptionOptions",
 ]
